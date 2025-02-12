0665183ced34e5a470256f405f6b78f7a90131fb
Tanishqaaa <khantedtanishqa22@gmail.com>
2025-02-12 16:04:09 +0100

    changes

--- main.m
+++ main.m
@@ -2,8 +2,10 @@ clc;
 clear;
 close all;
 
+% initialise the parameters
 parameters;
 
+% Not needed but can do to simplify variable names
 x0 = initialState;
 P0 = stateCovariance;
 Q = processNoise;
@@ -11,4 +13,8 @@ R = measurementNoise;
 dt = time;
 realMeasurements = z;
 
+% create an instance
 ukf = unscentedKalmanFilter(x0,P0,Q,R,dt);
+
+% create xUpdate and pUpdate
+[xUpdate,pUpdate] = ukf.updateStep(ukf,realMeasurements);
